07d8789a7d7bb7a8a55a952870d7480e0aded070
sky <sky@qq.com>
2019-05-23 10:38:08 +0800

    update lwip to add hostname in dhcp discovery

--- config/product_config.h
+++ config/product_config.h
@@ -23,6 +23,6 @@
 /* 版本 */
 #define VERSION_MAJOR   0
 #define VERSION_MINOR   2
-#define VERSION_PATCH   10
+#define VERSION_PATCH   11
                                                                  
 #endif
